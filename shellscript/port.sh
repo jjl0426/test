1f69b148375842f4a31d3266c86d47a982327381
jjl0426 <1465642066@qq.com>
2020-01-12 23:11:19 -0800

    本机Linux所有开放的端口列表

---
+++ shellscript/port.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Author:
+#Date & Time:2020-01-12 23:08:54
+#Description:显示本机Linux系统上所有开放的端口列表
+
+# 从端口列表中观测有没有没用的端口,有的话可以将该端口对应的服务关闭,防止意外的攻击可能性
+ss -nutlp | awk '{print $1,$5}' | awk -F"[: ]" '{print "协议:"$1,"端口号:"$NF}' | grep "[0‐9]" | uniq
